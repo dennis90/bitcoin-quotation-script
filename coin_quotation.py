d36c7c03f2e87030f911e08350312ad062f7bd10
Dennis Alves <dennis.alves@live.com>
2016-08-12 10:21:32 -0300

    changing from tabs to spaces

--- coin_quotation.py
+++ coin_quotation.py
@@ -10,35 +10,35 @@ class CoinQuotation:
                 'foxbit': 'https://api.blinktrade.com/api/v1/BRL/ticker?crypto_currency=BTC',
                 'mercado': 'https://www.mercadobitcoin.net/api/v2/ticker/',
                 'bitstamp': 'https://www.bitstamp.net/api/ticker/',
-				'okcoin': 'https://www.okcoin.com/api/v1/ticker.do?symbol=btc_usd',
-				'bitfinex': 'https://api.bitfinex.com/v1/pubticker/btcusd',
-				'coindesk': 'http://api.coindesk.com/v1/bpi/currentprice.json',
-				'b2u': 'https://www.bitcointoyou.com/api/ticker.aspx',
+                'okcoin': 'https://www.okcoin.com/api/v1/ticker.do?symbol=btc_usd',
+                'bitfinex': 'https://api.bitfinex.com/v1/pubticker/btcusd',
+                'coindesk': 'http://api.coindesk.com/v1/bpi/currentprice.json',
+                'b2u': 'https://www.bitcointoyou.com/api/ticker.aspx',
             }
 
         self.path_json = {
-        			 'usd': 'valores/USD/valor',
-        			 'foxbit': 'last',
-					 'mercado': 'ticker/last',
-					 'bitstamp': 'last',
+                     'usd': 'valores/USD/valor',
+                     'foxbit': 'last',
+                     'mercado': 'ticker/last',
+                     'bitstamp': 'last',
                      'okcoin': 'ticker/last',
-					 'bitfinex': 'last_price',
-					 'coindesk': 'bpi/USD/rate',
-					 'b2u': 'ticker/last',
+                     'bitfinex': 'last_price',
+                     'coindesk': 'bpi/USD/rate',
+                     'b2u': 'ticker/last',
             }
 
     def value_from_json(self, json, exchange):
-    	path = self.path_json[exchange]
-    	json_child = json
-    	for idx in path.split('/'):
-    		json_child = json_child[idx]
-    	return json_child
+        path = self.path_json[exchange]
+        json_child = json
+        for idx in path.split('/'):
+            json_child = json_child[idx]
+        return json_child
 
     def url_to_json(self, url):
-    	return json.load(urllib2.urlopen(url))
+        return json.load(urllib2.urlopen(url))
 
     def get_quotation(self, exchange):
-    	url = self.urls[exchange]
-    	json_file = self.url_to_json(url)
-    	value = self.value_from_json(json_file, exchange)
-    	return float(value)
+        url = self.urls[exchange]
+        json_file = self.url_to_json(url)
+        value = self.value_from_json(json_file, exchange)
+        return float(value)
--- loader.py
+++ loader.py
@@ -8,53 +8,53 @@ moedas = CoinQuotation()
 reimprime = "S"
 
 while reimprime == "S":
-	# Imprime data / hora da consulta
-	print("Hora da Consulta: %s" % datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+    # Imprime data / hora da consulta
+    print("Hora da Consulta: %s" % datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 
-	# Imprime a cotação do dólar
-	print(u"Dólar Comercial: %s" % moedas.get_quotation('usd'))
-	print("=" * 30)
+    # Imprime a cotação do dólar
+    print(u"Dólar Comercial: %s" % moedas.get_quotation('usd'))
+    print("=" * 30)
 
-	# Imprime a cotação do bitcoin nas exchanges brasileiras
-	print("\tBITCOIN EXCHANGES BRASILEIRAS")
+    # Imprime a cotação do bitcoin nas exchanges brasileiras
+    print("\tBITCOIN EXCHANGES BRASILEIRAS")
 
-	foxbit = moedas.get_quotation('foxbit')
-	mercado = moedas.get_quotation('mercado')
-	b2u  = moedas.get_quotation('b2u')
+    foxbit = moedas.get_quotation('foxbit')
+    mercado = moedas.get_quotation('mercado')
+    b2u  = moedas.get_quotation('b2u')
 
-	menor = "Foxbit" if foxbit <= mercado and foxbit <= b2u \
-				else "Mercado Bitcoin" if mercado <= b2u else "Bitcoin 2 You"
+    menor = "Foxbit" if foxbit <= mercado and foxbit <= b2u \
+                else "Mercado Bitcoin" if mercado <= b2u else "Bitcoin 2 You"
 
-	print("       Foxbit : %.3f" % foxbit)
-	print("  Mercado BTC : %.3f" % mercado)
-	print("Bitcoin 2 You : %.3f" % b2u)
-	print("")
-	print(u"Menor Preço Brasil: %s" % menor)
-	print("=" * 30)	
+    print("       Foxbit : %.3f" % foxbit)
+    print("  Mercado BTC : %.3f" % mercado)
+    print("Bitcoin 2 You : %.3f" % b2u)
+    print("")
+    print(u"Menor Preço Brasil: %s" % menor)
+    print("=" * 30)    
 
-	# Imprime a cotação do bitcoin nas exchanges internacionais
-	print("\tBITCOIN EXCHANGES INTERNACIONAIS")
+    # Imprime a cotação do bitcoin nas exchanges internacionais
+    print("\tBITCOIN EXCHANGES INTERNACIONAIS")
 
-	bitfinex = moedas.get_quotation('bitfinex')
-	bitstamp = moedas.get_quotation('bitstamp')
-	coindesk = moedas.get_quotation('coindesk')
-	okcoin = moedas.get_quotation('okcoin')	
+    bitfinex = moedas.get_quotation('bitfinex')
+    bitstamp = moedas.get_quotation('bitstamp')
+    coindesk = moedas.get_quotation('coindesk')
+    okcoin = moedas.get_quotation('okcoin')    
 
-	menor = "Bitfinex" if bitfinex <= bitstamp and bitfinex <= coindesk  and bitfinex <= okcoin \
-				else "Bitstamp" if bitstamp <= coindesk and bitstamp <= okcoin \
-				else "Coin Desk" if coindesk <= okcoin \
-				else "Ok Coin"
+    menor = "Bitfinex" if bitfinex <= bitstamp and bitfinex <= coindesk  and bitfinex <= okcoin \
+                else "Bitstamp" if bitstamp <= coindesk and bitstamp <= okcoin \
+                else "Coin Desk" if coindesk <= okcoin \
+                else "Ok Coin"
 
-	print("     Bitfinex : %.3f" % bitfinex)
-	print("     Bitstamp : %.3f" % bitstamp)
-	print("    Coin Desk : %.3f" % coindesk)
-	print("      Ok Coin : %.3f" % okcoin)
-	print("")
-	print(u"Menor Preço Internacional: %s" % menor)
-	print("=" * 30)
-	print("\n\n")
+    print("     Bitfinex : %.3f" % bitfinex)
+    print("     Bitstamp : %.3f" % bitstamp)
+    print("    Coin Desk : %.3f" % coindesk)
+    print("      Ok Coin : %.3f" % okcoin)
+    print("")
+    print(u"Menor Preço Internacional: %s" % menor)
+    print("=" * 30)
+    print("\n\n")
 
-	reimprime = raw_input('Atualizar? (S/N)').upper()
+    reimprime = raw_input('Atualizar? (S/N)').upper()
 
-	print("*" * 50)
-	print("\n\n")
+    print("*" * 50)
+    print("\n\n")
